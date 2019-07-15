b409f62fab5fe085089736c2747b21c31c040ee6
Florian Dahlitz <f2dahlitz@freenet.de>
2019-07-15 14:01:30 +0200

    feat(code): add exercise_07 with solution

---
+++ code/exercises/exercise_07.py
@@ -0,0 +1,3 @@
+# Print the sum of all numbers inside the provided list.
+# Expected output: 31
+a = [1, 2, 3, 4, 10, 11]
---
+++ code/solutions/solution_07.py
@@ -0,0 +1,4 @@
+# This is a possible solution for exercise_07.py
+a = [1, 2, 3, 4, 10, 11]
+
+print(sum(a))
