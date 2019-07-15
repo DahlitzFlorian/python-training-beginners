f8a4bc1c90b2f7d24449707189a1fe63f5efb02c
Florian Dahlitz <f2dahlitz@freenet.de>
2019-07-15 11:33:08 +0200

    feat(code): add exercise_03 with solution

---
+++ code/exercises/exercise_03.py
@@ -0,0 +1,2 @@
+# Print only the elements of the following list a, that are less or equal to 10.
+a = [1, 2, 9, 21, 42, 3, 1, 55, 8, 0, -1, 32]
---
+++ code/solutions/solution_03.py
@@ -0,0 +1,6 @@
+# This is a possible solution for exercise_03.py
+a = [1, 2, 9, 21, 42, 3, 1, 55, 8, 0, -1, 32]
+
+for element in a:
+    if element <= 10:
+        print(element)
